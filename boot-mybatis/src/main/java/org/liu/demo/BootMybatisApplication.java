9f9e9cfa69a26e8b57a1f4df9a63e6e465db16d6
RomanticWd <402468285@qq.com>
2020-04-16 22:55:23 +0800

    仓库初始化&第一个mybatis的demo

---
+++ boot-mybatis/src/main/java/org/liu/demo/BootMybatisApplication.java
@@ -0,0 +1,13 @@
+package org.liu.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootMybatisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootMybatisApplication.class, args);
+    }
+
+}
