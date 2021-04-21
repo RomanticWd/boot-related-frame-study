444004472c34721e4e72d73c5b4c6b97ff47f3af
yue.liu <402468285@qq.com>
2021-04-21 12:25:09 +0800

    springboot集成ldap认证-补充

---
+++ boot-ldap/src/main/java/org/liu/demo/ldap/BootLdapApplication.java
@@ -0,0 +1,13 @@
+package org.liu.demo.ldap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootLdapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootLdapApplication.class, args);
+	}
+
+}
